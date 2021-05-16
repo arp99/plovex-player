f51f05555b2cbb7fba1c052bf7f668d8f54e51f3
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-16 18:52:50 +0530

    Add video database

---
+++ plovex-player/src/Video-DB/VideoDb.js
@@ -0,0 +1,26 @@
+export const videos = [
+    {
+        id:"bBcxVRaj_V4",
+        title:"BELGRADE TO NIS TRAIN IN SERBIA 🇷🇸 | LET'S GO TO SKI",
+        thumbnail:"https://img.youtube.com/vi/bBcxVRaj_V4/hq720.jpg",
+        url:"https://www.youtube.com/watch?v=bBcxVRaj_V4"
+    },
+    {
+        id:"5ZqID5bSPug",
+        title:"LIFE LEARNING FROM JAPANESE CHERRY BLOSSOM II HINDI II Rom Rom Ji",
+        thumbnail:"https://img.youtube.com/vi/5ZqID5bSPug/hq720.jpg",
+        url:"https://www.youtube.com/watch?v=5ZqID5bSPug"
+    },
+    {
+        id:"wyuezv4qfE8",
+        title:"Discover Mallorca with Dhruv Rathee | Travel Tips for Mallorca in the Pandemic",
+        thumbnail:"https://img.youtube.com/vi/wyuezv4qfE8/hq720.jpg",
+        url:"https://www.youtube.com/watch?v=wyuezv4qfE8"
+    },
+    {
+        id:"41dSPg4X3x8",
+        title:"UNIQUE HOUSE BOAT EXPERIENCE IN ALLEPPY, KERALA",
+        thumbnail:"https://img.youtube.com/vi/41dSPg4X3x8/hq720.jpg",
+        url:"https://www.youtube.com/watch?v=41dSPg4X3x8"
+    },
+]
