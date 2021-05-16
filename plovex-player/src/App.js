75bb6c45cbfbfd50873a294f9cb329c5ba215449
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-16 19:00:23 +0530

    Add react router
    Add react router and create home route

--- plovex-player/src/App.js
+++ plovex-player/src/App.js
@@ -1,10 +1,14 @@
 import { Navbar } from "./Components"
+import { Routes , Route } from "react-router-dom"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <Navbar />
+      <Routes>
+        <Route path="/"  element={<Videolisting />}/>
+      </Routes>
     </div>
   );
 }
--- plovex-player/src/index.js
+++ plovex-player/src/index.js
@@ -1,15 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
+import { BrowserRouter as Router } from "react-router-dom"
 import App from './App';
 import { Videosprovider } from "./Context"
 
 
 ReactDOM.render(
   <React.StrictMode>
+    <Router>
       <Videosprovider>
         <App />
       </Videosprovider>
+    </Router>
   </React.StrictMode>,
   document.getElementById('root')
 );
