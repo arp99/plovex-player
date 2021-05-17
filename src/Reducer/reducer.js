a53140af88be0e8a816cc083ca805c1dd1be64bf
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-17 18:20:17 +0530

    Refactor : Pass only videoId to payload

--- src/Reducer/reducer.js
+++ src/Reducer/reducer.js
@@ -1,27 +1,26 @@
 import { v4 as uuid } from "uuid"
 
 export const reducer  = (prevState , { type , payload}) =>{
-    const { id } = payload;
     console.log(payload)
     switch(type){
-        case "ADD_TO_WATCH_LATER" :
-            const isPresentInWatchLAter =  prevState.watchLater.find(video => video.id === id) 
+        case "ADD_TO_WATCHLATER" :
+            const isPresentInWatchLAter =  prevState.watchLater.find(id => id === payload.videoId) 
             if(!isPresentInWatchLAter){
-                return {...prevState , watchLater: [...(prevState.watchLater) , payload]}
+                return {...prevState , watchLater: [...(prevState.watchLater) , payload.videoId]}
             }
             return prevState;
         case "TOGGLE_LIKE": 
-            const isLiked = prevState.liked.find(video => video.id === id)
+            const isLiked = prevState.liked.find(id => id === payload.videoId)
             if(!isLiked){
-                return {...prevState , liked: [...(prevState.liked) , payload]}
+                return {...prevState , liked: [...(prevState.liked) , payload.videoId]}
             }
             else{
-                return {...prevState , liked: prevState.liked.filter(video => video.id !== id)}
+                return {...prevState , liked: prevState.liked.filter(id => id !== payload.videoId)}
             }
         case "ADD_TO_HISTORY":
-            const isPresentInHistory =  prevState.history.find(video => video.id === id)
+            const isPresentInHistory =  prevState.history.find(id => id === payload.videoId)
             if(!isPresentInHistory){
-                return { ...prevState , history: [ ...(prevState.history) , payload ]}
+                return { ...prevState , history: [ ...(prevState.history) , payload.videoId ]}
             }
             return prevState
         case "TOGGLE_VIDEO_IN_PLAYLIST": 
