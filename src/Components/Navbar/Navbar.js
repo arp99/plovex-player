7310d3deb4cd6a8c51a8d1a8ecfef20619818b79
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-17 21:35:44 +0530

    Style: Moved navbar to bottom
    Moved navbar to the bottom for mobile view

--- src/Components/Navbar/Navbar.js
+++ src/Components/Navbar/Navbar.js
@@ -1,45 +1,46 @@
 import "./Navbar.css"
-import { useState } from "react"
 import { Link } from "react-router-dom"
-import { MdMenu , MdClose} from "react-icons/md"
+import { MdHome , MdHistory , MdPlaylistAdd , MdWatchLater , MdFavorite } from "react-icons/md"
+import { IconContext } from "react-icons";
 
 export const Navbar = () =>{
-    const [ showNav , setShowNav ] = useState(false)
+    const iconStyle = {
+        color:"turquoise",
+        size:"2rem"
+    }
     return(
         <nav className="navbar__container">
-            <div className="navbar__header">
-                <div className="navbar__brand">Plovex-player</div>
-                <button 
-                    className="btn" 
-                    onClick={()=>setShowNav(prev => !prev)}
-                >
-                    {showNav?<MdClose />:<MdMenu />}
-                </button>
-            </div>
             <div 
-                className={showNav ?"navbar__list-items show" :"navbar__list-items"} >
-                <ul className="list__container">
-                    <li className="list__item">
-                        <Link to="/playlist">
-                            Playlist
-                        </Link>
-                    </li>
-                    <li className="list__item">
-                        <Link to="/watchlater">
-                            Watchlater
-                        </Link>
-                    </li>
-                    <li className="list__item">
-                        <Link to="/liked">
-                            Liked
-                        </Link>
-                    </li>
-                    <li className="list__item">
-                        <Link to="/history">
-                            History
-                        </Link>
-                    </li>
-                </ul>
+                className="navbar__list-items" >
+                <IconContext.Provider value={iconStyle} >
+                    <ul className="list__container">
+                        <li className="list__item">
+                            <Link to="/">
+                                <MdHome />
+                            </Link>
+                        </li>
+                        <li className="list__item">
+                            <Link to="/playlist">
+                                <MdPlaylistAdd />
+                            </Link>
+                        </li>
+                        <li className="list__item">
+                            <Link to="/watchlater">
+                                <MdWatchLater />
+                            </Link>
+                        </li>
+                        <li className="list__item">
+                            <Link to="/liked">
+                                <MdFavorite />
+                            </Link>
+                        </li>
+                        <li className="list__item">
+                            <Link to="/history">
+                                <MdHistory />
+                            </Link>
+                        </li>
+                    </ul>
+                </IconContext.Provider>
             </div>
         </nav>
     )
