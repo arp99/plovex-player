edae6d74b7275613bac5f31f25cb7e29097b69e9
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-24 15:58:15 +0530

    Style: Update styles of watchlater page

--- src/Pages/Watchlater/Watchlater.js
+++ src/Pages/Watchlater/Watchlater.js
@@ -1,18 +1,52 @@
 import { useVideos } from "../../Context"
-
+import { Link } from "react-router-dom"
+import { IconContext } from "react-icons";
+import { RiDeleteBin6Line } from "react-icons/ri";
+import "./Watchlater.css"
 export const Watchlater = () =>{
-    const { state } = useVideos()
+    const { state , dispatch } = useVideos()
     const videoData = state.watchLater.map(id => state.videos.find(video => video.id === id))
     console.log(videoData)
+
+    const iconStyle = {
+        color:"turquoise",
+        size:"1.5rem"
+    }
+
     return(
         <div>
+            <h1>Watchlater</h1>
             {
-                videoData.map(({id , title})=>{
-                    return(
-                        <p key={id}>{title}</p>
-                    )
-                })
+                videoData.length === 0 ?<small>No videos in Watchlater</small>:null
             }
+            <div className="watchlater-videos">
+                {
+                    videoData.map(({id:videoId , title , thumbnail , url })=>{
+                        return(
+                            <div className="watchlater-video__container">
+                                <div className="watchlater-thumbnail__container" key={videoId}> 
+                                    <Link to ={`/v/${videoId}`} >
+                                        <img 
+                                            src={ thumbnail } 
+                                            alt="sample" 
+                                            className="card-img" 
+                                        />
+                                    </Link>
+                                </div>
+                                <div className="watchlater-description__container">
+                                    <p>{title}</p>
+                                    <IconContext.Provider value={iconStyle} >
+                                        <RiDeleteBin6Line 
+                                            onClick={()=>dispatch({type: "REMOVE_FROM_WATCHLATER" , payload:{ videoId }})}
+                                            className="btn-action"
+                                        />                                    
+                                    </IconContext.Provider>
+                                </div>
+                            </div>
+                        )
+                    })
+                }
+            </div>
         </div>
     )
 }
