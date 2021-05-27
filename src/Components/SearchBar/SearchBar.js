7c3320cd54af92fea956042aa8a4d81abd22931b
Arpan Mondal <47218246+arp99@users.noreply.github.com>
2021-05-27 14:17:00 +0530

    Feat: Show SearchBox on scroll up only
    Toggle the searchBox when scrolling down and up

--- src/Components/SearchBar/SearchBar.js
+++ src/Components/SearchBar/SearchBar.js
@@ -1,9 +1,27 @@
+import { useEffect, useState } from "react"
 import { useSearchData } from "../../Context"
 import searchBarStyle from "./SearchBar.module.css"
 export const SearchBar = () =>{
     const { searchStr , setSearchStr } = useSearchData()
+    const [ prevScrollPos , setPrevScrollPos ] = useState(0)
+    const [ visible , setVisible ] = useState(true)
+
+    useEffect(()=>{
+        const handleScroll = () =>{
+            //find current scroll position
+            const currScrollPos = window.pageYOffset;
+            //set the visible state based on the location of scroll
+            setVisible(prevScrollPos > currScrollPos)
+            //set the current scroll position
+            setPrevScrollPos(currScrollPos)
+        }
+        window.addEventListener('scroll', handleScroll)
+        return ()=>{
+            window.removeEventListener('scroll', handleScroll)
+        }
+    },[visible , prevScrollPos ])
     return(
-        <div className={searchBarStyle.searchBox__container}>
+        <div className={visible?`${searchBarStyle.searchBox__container}`: `${searchBarStyle.searchBox__container} ${searchBarStyle.hidden}`}>
             <input 
                 value={searchStr}
                 placeholder={"Search Videos?"}
